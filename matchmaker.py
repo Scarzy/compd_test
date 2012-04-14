e7eeb3616978c9735c5da5675d8eb3e57953b7b8
Thomas Scarsbrook <scarzybrook@gmail.com>
2012-04-14 02:41:02 +0100

    Update to the new match spec

--- matchmaker.py
+++ matchmaker.py
@@ -1,15 +1,16 @@
 #!/bin/env python
 
-import redis
+import redis, json
 from random import choice
 
 teams = ['ARG','BWS','BWS2','BGR','BRK','BSG','CLF','CLF2','EMM','GRS','GMR','HRS','HZW','MFG','MUC','PSC','PSC2','QEH','QMC','SEN','SEN2','TTN']
-matches = ["3600,PSC,MFG,BSG,QMC,1"]
+matches = ["{'time':3600,'teams': ['PSC','MFG','BSG','QMC'], 'delay': 1}"]
 
 actor = redis.Redis(host='localhost',port=6379,db=0)
 
 for i in range(20):
-	str = '{0},{1},{2},{3},{4},{5}'.format((3600 + (7*(i+1))),choice(teams),choice(teams),choice(teams),choice(teams),i+2)
+	obj = {'time': (3600 + (7*60*(i+1))),'teams':[choice(teams),choice(teams),choice(teams),choice(teams)],'delay':3}
+	str = json.dumps(obj)
 	matches.append(str)
 
 for i in range(21):
